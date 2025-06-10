1ddb42c05523f2f27fa02f7d76fdfdbf427f5f7e
n.gnato <n.gnato@ya.ru>
2025-06-10 20:58:34 +0300

    Use fast route implementation

--- src/FreeElephants/PsrRouter/Router.php
+++ src/FreeElephants/PsrRouter/Router.php
@@ -3,18 +3,42 @@ declare(strict_types=1);
 
 namespace FreeElephants\PsrRouter;
 
+use FastRoute\Dispatcher;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class Router
 {
 
-    public function __construct()
+    private RequestHandlerFactoryInterface $requestHandlerFactory;
+    private Dispatcher $dispatcher;
+
+    public function __construct(
+        RequestHandlerFactoryInterface $requestHandlerFactory,
+        Dispatcher                     $dispatcher
+    )
     {
+        $this->requestHandlerFactory = $requestHandlerFactory;
+        $this->dispatcher = $dispatcher;
     }
 
-    public function getHandler(ServerRequestInterface  $request): RequestHandlerInterface
+    public function getHandler(ServerRequestInterface $request): RequestHandlerInterface
     {
+        $fastRouteResult = $this->dispatcher->dispatch($request->getMethod(), $request->getUri()->getPath());
+        switch ($fastRouteResult[0]) {
+            case Dispatcher::FOUND:
+                $args = $fastRouteResult[2];
+                foreach ($args as $name => $value) {
+                    $request->withAttribute($name, $value);
+                }
 
+                return $this->requestHandlerFactory->create($fastRouteResult[1]);
+            case Dispatcher::NOT_FOUND:
+                throw new \Exception('Route not found');
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                throw new \Exception('Method not allowed');
+        }
+
+        throw new \Exception('Unexpected fast route result');
     }
 }
--- tests/FreeElephants/PsrRouter/RouterTest.php
+++ tests/FreeElephants/PsrRouter/RouterTest.php
@@ -3,19 +3,31 @@ declare(strict_types=1);
 
 namespace FreeElephants\PsrRouter;
 
+use FastRoute\RouteCollector;
 use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Server\RequestHandlerInterface;
+use function FastRoute\simpleDispatcher;
 
 class RouterTest extends TestCase
 {
     public function testGetHandler(): void
     {
-        $router = new Router();
-        try {
+        $dispatcher = simpleDispatcher(function(RouteCollector $r) {
+            // {id} must be a number (\d+)
+            $r->addRoute('GET', '/users/{id:\d+}', 'get_user_handler');
+        });
 
-            $router->getHandler(new ServerRequest('GET', '/'));
-        } catch (\TypeError $e) {
-            
-        }
+        $requestHandlerFactory = $this->createMock(RequestHandlerFactoryInterface::class);
+        $expectedRequestHandler = $this->createMock(RequestHandlerInterface::class);
+        $requestHandlerFactory->expects($this->once())->method('create')->with('get_user_handler')->willReturn($expectedRequestHandler);
+        $router = new Router(
+            $requestHandlerFactory,
+            $dispatcher,
+        );
+
+        $serverRequest = new ServerRequest('GET', '/users/100');
+        $actualRequestHandler = $router->getHandler($serverRequest);
+        $this->assertSame($expectedRequestHandler, $actualRequestHandler);
     }
 }
